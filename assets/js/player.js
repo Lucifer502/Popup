8d41c02756048d3eedc8e6a8dc1b46a730f836f4
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-19 17:20:56 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -21,6 +21,33 @@ const r = { 0: '720p', 1: '1080p', 2: '480p', 3: '360p', 4: '240p' };
   sources.push({ file: video_mp4_array[idx], label: r[idx] + (idx < 2 ? '<sup><sup>HD</sup></sup>' : '') });
  startPlayer();
 
+function linkDownload(id, tentativas = 0) {
+    console.log('  - Baixando: ', r[id])
+    let video_mp4_url = video_mp4_array[id];
+
+    let fileSize = "";
+    let http = (window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"));
+    http.onreadystatechange = () => {
+      if (http.readyState == 4 && http.status == 200) {
+        fileSize = http.getResponseHeader('content-length');
+        if (!fileSize)
+          return setTimeout(() => linkDownload(id), 5000);
+        else {
+          let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+          if (fileSize == 0) return console.log('addSource#fileSize == 0');
+          let i = parseInt(Math.floor(Math.log(fileSize) / Math.log(1024)));
+          if (i == 0) return console.log('addSource#i == 0');
+          let return_fileSize = (fileSize / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+          dlSize[id].innerText = return_fileSize;
+          return console.log(`[CR Premium] Source adicionado: ${r[id]} (${return_fileSize})`);
+        }
+      } else if (http.readyState == 4 && tentativas < 3)
+        return setTimeout(() => linkDownload(id, tentativas + 1), 5000);
+    }
+    http.open("HEAD", video_mp4_url, true);
+    http.send(null);
+  }
+
  function startPlayer() {
 
   let playerInstance = jwplayer('player_div')
