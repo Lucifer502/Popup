de719ada0b086fc4c2ce1825482cfc341ff9f1a5
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-19 18:59:44 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,98 +1,90 @@
 window.addEventListener('message', async e => {
-
-const r = { 0: '720p', 1: '1080p', 2: '480p', 3: '360p', 4: '240p' };
-
+ let r = { 0: '720p', 1: '1080p', 2: '480p', 3: '360p', 4: '240p' };
+ let allorigins = "https://crp-proxy.herokuapp.com/get?url=";
  let streamrgx = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
  let streamrgx_three = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
- let allorigins = "https://crp-proxy.herokuapp.com/get?url=";
-
- let video_config_media = await getStreams(e.data.config_media);
- let url = e.data.href;
+ let video_config_media = await getconfigMedia(e.data.config_media);
  let video_mp4_array = [];
  let user_lang = 'es-LA';
+ let url = e.data.href;
  let sources = [];
 
- console.log(video_config_media)
+ let dlSize = [];
+ let dlUrl = [];
+ for (let idx in r) {
+  dlSize[idx] = document.getElementById(r[idx] + "_down_size");
+  dlUrl[idx] = document.getElementById(r[idx] + "_down_url");
+ };
 
- const streamlist = video_config_media['streams']
- video_mp4_array = mp4ListFromStream(streamlist.adaptive_hls[user_lang].url);
+ video_mp4_array = mp4ListFromStream(video_config_media.streams.adaptive_hls[user_lang].url);
 
  for (let idx of [1, 0, 2, 3, 4])
   sources.push({ file: video_mp4_array[idx], label: r[idx] + (idx < 2 ? '<sup><sup>HD</sup></sup>' : '') });
  startPlayer();
 
-function linkDownload(id, tentativas = 0) {
-    console.log('  - Baixando: ', r[id])
-    let video_mp4_url = video_mp4_array[id];
+ function downloadStreams(id, intentos = 0) {
+  let video_mp4_url = video_mp4_array[id];
 
-    let fileSize = "";
-    let http = (window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"));
-    http.onreadystatechange = () => {
-      if (http.readyState == 4 && http.status == 200) {
-        fileSize = http.getResponseHeader('content-length');
-        if (!fileSize)
-          return setTimeout(() => linkDownload(id), 5000);
-        else {
-          let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-          if (fileSize == 0) return console.log('addSource#fileSize == 0');
-          let i = parseInt(Math.floor(Math.log(fileSize) / Math.log(1024)));
-          if (i == 0) return console.log('addSource#i == 0');
-          let return_fileSize = (fileSize / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
-          dlSize[id].innerText = return_fileSize;
-          return console.log(`[CR Premium] Source adicionado: ${r[id]} (${return_fileSize})`);
-        }
-      } else if (http.readyState == 4 && tentativas < 3)
-        return setTimeout(() => linkDownload(id, tentativas + 1), 5000);
+  let fileSize = "";
+  let http = (window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"));
+  http.onreadystatechange = () => {
+   if (http.readyState == 4 && http.status == 200) {
+    fileSize = http.getResponseHeader('content-length');
+    if (!fileSize)
+     return setTimeout(() => downloadStreams(id), 5000);
+    else {
+     let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+     if (fileSize == 0) return console.log('addSource#fileSize == 0');
+     let i = parseInt(Math.floor(Math.log(fileSize) / Math.log(1024)));
+     if (i == 0) return console.log('addSource#i == 0');
+     let return_fileSize = (fileSize / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+     dlSize[id].innerText = return_fileSize;
+     return console.log(`[CR Premium] Source adicionado: ${r[id]} (${return_fileSize})`);
     }
-    http.open("HEAD", video_mp4_url, true);
-    http.send(null);
+   } else if (http.readyState == 4 && intentos < 3)
+    return setTimeout(() => downloadStreams(id, intentos++), 5000);
   }
+  http.open("HEAD", video_mp4_url, true);
+  http.send(null);
+ };
 
  function startPlayer() {
-
   let playerInstance = jwplayer('player_div')
   playerInstance.setup({
    'playlist': [{
     'sources': sources,
   }]
-  })
+  });
 
-let download_iconPath = "assets/icon/download_icon.svg";
-    let download_id = "download-video-button";
-    let download_tooltipText = "Download";
+  let download_iconPath = "assets/icon/download_icon.svg";
+  let download_id = "download-video-button";
+  let download_tooltipText = "Download";
 
-function download_Button() {
-      if (jwplayer().getEnvironment().OS.mobile == true) {
-        modal.style.height = "200px";
-        modal.style.overflow = "auto";
-      }
-      openModal.style.opacity = "1";
-      openModal.style.visibility = "visible";
+  const openModal = document.querySelectorAll(".modal-container")[0];
+  const modal = document.querySelectorAll(".modal")[0];
+  const closeModal = document.querySelectorAll(".close")[0];
 
-closeModal.addEventListener("click", function(e) {
-        openModal.style.opacity = "0";
-        openModal.style.visibility = "hidden"
-      })
-    }
+  function download_Button() {
+   openModal.style.opacity = "1";
+   openModal.style.visibility = "visible";
+  };
 
-playerInstance.addButton(download_iconPath, download_tooltipText, download_Button, download_id);
+  closeModal.addEventListener("click", function(e) {
+   openModal.style.opacity = "0";
+   openModal.style.visibility = "hidden"
+  });
 
-console.log('[CR Premium] Baixando sources:')
-      for (let id of [1, 0, 2, 3, 4])
-        linkDownload(id);
-// Definir URL e Tamanho na lista de download
-      for (let id of [1, 0, 2, 3, 4]) {
-        dlUrl[id].href = video_mp4_array[id];
-}
+  playerInstance.addButton(download_iconPath, download_tooltipText, download_Button, download_id);
 
+  for (let id of [1, 0, 2, 3, 4]) {
+   dlUrl[id].href = video_mp4_array[id];
+   downloadStreams(id);
+  };
 
   jwplayer().on('ready', e => {
-
    document.body.querySelector(".loading_container").style.display = "none";
   });
- }
-
-
+ };
 
  function getAllOrigins(url) {
   return new Promise(async (resolve, reject) => {
@@ -107,21 +99,21 @@ console.log('[CR Premium] Baixando sources:')
     })
     .catch(err => reject(err));
   })
- }
+ };
 
- async function getStreams(url) {
-  const episodeStream = JSON.parse(await getAllOrigins(url));
-  return episodeStream;
- }
+ async function getconfigMedia(url) {
+  let config_media = JSON.parse(await getAllOrigins(url));
+  return config_media;
+ };
 
  function mp4ListFromStream(url) {
   const cleanUrl = url.replace('evs1', 'evs').replace(url.split("/")[2], "fy.v.vrv.co");
   const res = [];
   for (let i in r)
-   if (streamrgx_three.test(cleanUrl) && i <= 2) // por algum motivo alguns videos da CR tem apenas 3 resoluções
+   if (streamrgx_three.test(cleanUrl) && i <= 2)
     res.push(cleanUrl.replace(streamrgx_three, `_$${(parseInt(i)+1)}`))
   else
    res.push(cleanUrl.replace(streamrgx, `_$${(parseInt(i)+1)}`))
   return res;
- }
+ };
 });
